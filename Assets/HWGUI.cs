7def6c3baa262bef00ef0afd1d3275d8288175fd
Rico <wolbereric@yahoo.fr>
2019-02-22 15:50:13 +0100

    Demo

---
+++ Assets/HWGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using SocketIO;
+
+public class HWGUI : MonoBehaviour
+{
+    private SocketIOComponent socket;
+
+    private void Start()
+    {
+        socket = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
+    }
+
+    private void OnGUI()
+    {
+        if (Debug.isDebugBuild)
+        {
+            GUI.contentColor = Color.black;
+            GUI.Label(new Rect(10, 110, 350, 100), "Socket connect : " + socket.url);
+        }
+    }
+}
